feb9f8434fdb7d76eac248fcec0011589ad1605a
coder_sam <sanathwaghela1@gmail.com>
2025-01-07 16:33:42 +0530

    added debug image docker file and commands in script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,13 @@
 docker build -t python-distroless:latest -f Dockerfile-distroless .
 docker build -t python:latest -f Dockerfile-normal  .
+docker build -t python-distroless:debug -f Dockerfile-distroless-debug .
 
 trivy image python-distroless:latest
 trivy image python:latest
 
 docker run -d --name python-distroless-container -p 5001:5000 python-distroless:latest
 docker run -d --name python-container -p 5002:5000 python:latest
+docker run -d --name python-distroless-debug-container -p 5003:5000 python-distroless:debug
 
 
 # to debug normal image
@@ -13,6 +15,9 @@ docker exec -it python-container sh
 
 # to debug distroless image
 docker exec -it python-distroless-container sh  # this will not work as distroless image does not have shell
+
+# to debug distroless image with debug tag
+docker exec -it python-distroless-debug-container sh
 
 # will use cdebug to debug distroless image on docker 
 cdebug exec -it --image=nixery.dev/shell/vim/ps/tshark/curl python-distroless-container
